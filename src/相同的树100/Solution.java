bcc816b75321379da0814d649d3e67b6e3fd834f
忱宝贝aa <zpyccc@foxmail.com>
2021-03-14 13:35:17 +0800

    dfs

---
+++ src/相同的树100/Solution.java
@@ -0,0 +1,32 @@
+package 相同的树100;
+
+/**
+ * @author zhao peng yu
+ * @version 1.0
+ * @date 2021/3/14 12:20
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+ }
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null){
+            return true;
+        }
+        if (p == null || q == null){
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+
+}
